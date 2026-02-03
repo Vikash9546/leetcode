fc4252b74f059ac0d27c5f88bd547be1fc9c03c1
Vikash9546 <vikashkumarmystyle@gmail.com>
2026-02-03 12:57:19 +0530

    Time: 5 ms (66.52%), Space: 23.1 MB (45.09%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -4,15 +4,11 @@ class Solution:
         idx=float("inf")
         dist = float('inf')
         i=0
-        c=0
         while i<n:
             if points[i][0]==x or points[i][1]==y:
-                c+=1
                 d=(abs(points[i][0]-x)+abs(points[i][1]-y))
                 if dist>d:
                     dist = d
-                    if c>1:
-                        idx=min(idx,i)
                     idx=i
             i+=1
         if dist==float("inf"):
