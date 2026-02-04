d5d46495cdadc5ea0bf62f990f357dd7c2d9ce28
Vikash9546 <vikashkumarmystyle@gmail.com>
2026-02-04 12:11:49 +0530

    Time: 9 ms (75.23%), Space: 36.5 MB (6.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,14 @@ class Solution:
         for i in nums:
             if i in d:
                 d[i]+=1
+                if d[i]>=2:
+                    return True
             else:
                 d[i]=1
-        for i in nums:
-            if d[i]>=2:
-                return True
         return False
+        # for i in nums:
+        #     if d[i]>=2:
+        #         return True
+        # return False
         
         
