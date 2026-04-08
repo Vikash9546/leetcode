a69a65d0818b079177cd8ea346abcf6112d8ad72
Vikash <vikashkumarmystyle@gmail.com>
2026-04-08 11:47:28 +0530

    Time: 10 ms (94.9%), Space: 21.2 MB (64.39%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,16 +1,6 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        d={}
-        for i in candyType:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        k=d.keys()
-        if len(k)>len(candyType)//2:
-            return len(candyType)//2
-        else:
-            return len(k)
+        return min(len(candyType)//2,len(set(candyType)))
 
 
         
