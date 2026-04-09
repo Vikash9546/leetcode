77f2905e4c60ba39a7ad5f0a8bfe984c9167cf69
Vikash <vikashkumarmystyle@gmail.com>
2026-04-09 12:32:14 +0530

    Time: 4 ms (25.46%), Space: 19.4 MB (24.72%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -1,9 +1,17 @@
 class Solution:
     def isThree(self, n: int) -> bool:
-        root = int(n**0.5)
-        if root*root!=n:
+        # root = int(n**0.5)
+        # if root*root!=n:
+        #     return False
+        # for i in range(2,root):
+        #     if root%i==0:
+        #         return False
+        # return root>1
+
+        if n==1 and n==2:
             return False
-        for i in range(2,root):
-            if root%i==0:
-                return False
-        return root>1
+        res=[1,n]
+        for i in range(2,n):
+            if n%i==0:
+                res.append(i)
+        return len(res)==3
